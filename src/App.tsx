dbb40cbed2251c423faf0661112b063df2951b93
Wectornanime <Wectornanime@gmail.com>
2023-08-07 21:32:41 -0300

    make component Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
 
+import Container from './components/Container'
+
 function App() {
 
   return (
     <>
+    <Container title='tti' description='desc'>
+      <p>ola</p>
+    </Container>
     </>
   )
 }
