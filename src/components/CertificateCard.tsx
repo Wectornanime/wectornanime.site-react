ff543bb55a92cdc5ae7f26a6ca875c7fd517b354
wectornanime <81574689+Wectornanime@users.noreply.github.com>
2023-08-18 12:22:53 +0000

    Update CertificateCard.tsx

--- src/components/CertificateCard.tsx
+++ src/components/CertificateCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './CertificateCard.css'
 import default_image from '../assets/images/no-image.png'
 
@@ -7,17 +8,18 @@ interface CertificateCardProps {
 }
 
 export default function CertificateCard(props:CertificateCardProps) {
-    let certificateImage:string
-    if (typeof(props.imageURL) === "undefined" || props.imageURL === "") {
-        certificateImage = default_image
-    } else {
-        certificateImage= props.imageURL as string;
-    }
+    const [error, setError] = useState(false);
+    
+    const handleImageError = () => {
+        setError(true);
+    };
 
     return (
         <li className="certificate-card">
             <div className="image-wrapper">
-                <img src={certificateImage} alt="Imagem do Certificado" />
+                {
+                    error ? (<img src={default_image} alt="Imagem do Certificado" />) : (<img src={props.imageURL} alt="Imagem do Certificado" onError={handleImageError} />)
+                }
             </div>
             <div className="certificate-card_content">
                 <p>{props.description}</p>
