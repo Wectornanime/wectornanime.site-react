0d436603b8baaff52727f1e60fdd37279c0b03b1
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2024-07-28 17:32:18 -0300

    update header location

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css'
 
-import Container from './components/Container'
-import ProjectCard from './components/ProjectCard'
-import CertificateCard from './components/CertificateCard'
-import TecBadge from './components/TecBadge'
+// imports
 import { useEffect, useState } from 'react'
 import { getData } from '@/services/api'
 import Data from './types/Data'
 
+// components
+import Header from './components/Header'
+import SocialMedia from './components/SocialMedia'
+import Container from './components/Container'
+import TecBadge from './components/TecBadge'
+import ProjectCard from './components/ProjectCard'
+import CertificateCard from './components/CertificateCard'
 
 function App() {
   const [data, setData] = useState<Data | null>(null)
@@ -19,79 +23,103 @@ function App() {
     })()
   })
   return (
-    <main>
+    <>
+      {data && (
+        <Header
+          name={data.name}
+          subName={data.subName}
+          imageURL={data.imageURL}
+        >
 
-      {
-        data && (
-          <Container
-            title="Sobre mim"
-            description={data.aboutMe}
-            containerClass="about-me"
-          />
-        )
+          {
+            data.links.map(item => {
+              return (
+                <SocialMedia
+                  label={item.label}
+                  url={item.url}
+                />
+              )
+            })
+          }
+
+        </Header>
+      )
       }
 
-      <Container
-        title="Habilidades"
-        description="Tecnologias que tenho habilidade."
-        containerClass="hability"
-      >
+      <main>
 
         {
-          data && data.hability.map(item => {
-            return (
-              <TecBadge
-                name={item.name}
-                deviconClass={item.deviconClass}
-              />
-            )
-          })
+          data && (
+            <Container
+              title="Sobre mim"
+              description={data.aboutMe}
+              containerClass="about-me"
+            />
+          )
         }
 
-      </Container>
+        <Container
+          title="Habilidades"
+          description="Tecnologias que tenho habilidade."
+          containerClass="hability"
+        >
 
-      <Container
-        title="Projetos"
-        description="Projetos que desenvolvi ao longo do tempo."
-        containerClass="projects"
-      >
+          {
+            data && data.hability.map(item => {
+              return (
+                <TecBadge
+                  name={item.name}
+                  deviconClass={item.deviconClass}
+                />
+              )
+            })
+          }
 
-        {
-          data && data.projects.map(item => {
-            return (
-              <ProjectCard
-                title={item.title}
-                description={item.description}
-                imageURL={item.imageURL}
-                links={item.links}
-              />
-            )
-          })
-        }
+        </Container>
 
-      </Container>
+        <Container
+          title="Projetos"
+          description="Projetos que desenvolvi ao longo do tempo."
+          containerClass="projects"
+        >
 
-      <Container
-        containerClass="certificates"
-        title="Certificados"
-        description="Certificados que conquistei durante o caminho."
-      >
+          {
+            data && data.projects.map(item => {
+              return (
+                <ProjectCard
+                  title={item.title}
+                  description={item.description}
+                  imageURL={item.imageURL}
+                  links={item.links}
+                />
+              )
+            })
+          }
 
-        {
-          data && data.certificates.map(item => {
-            return (
-              <CertificateCard
-                description={item.description}
-                imageURL={item.imageURL}
-                link={item.link}
-              />
-            )
-          })
-        }
+        </Container>
 
-      </Container>
+        <Container
+          containerClass="certificates"
+          title="Certificados"
+          description="Certificados que conquistei durante o caminho."
+        >
 
-    </main>
+          {
+            data && data.certificates.map(item => {
+              return (
+                <CertificateCard
+                  description={item.description}
+                  imageURL={item.imageURL}
+                  link={item.link}
+                />
+              )
+            })
+          }
+
+        </Container>
+
+      </main>
+    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,31 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import data from './assets/data.json'
-
-import Header from './components/Header.tsx'
-import SocialMedia from './components/SocialMedia.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Header
-    name={data.name}
-    subName={data.subName}
-    imageURL={data.imageURL}
-    >
-
-      {
-        data.links.map(item => {
-          return (
-            <SocialMedia
-            label={item.label}
-            url={item.url}
-            />
-          )
-        })
-      }
-
-    </Header>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
