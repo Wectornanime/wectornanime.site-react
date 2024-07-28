8d0e357e78cf7efa9c92e772c1d58f9a477d46d1
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2024-07-28 16:32:37 -0300

    Update Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,7 +4,7 @@ interface ContainerProps {
     title:string,
     description:string,
     containerClass:string
-    children?:JSX.Element | JSX.Element[]
+    children?:JSX.Element | JSX.Element[] | null
 }
 
 export default function Container(props:ContainerProps) {
